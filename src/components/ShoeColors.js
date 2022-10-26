c2987805b38c577860ad2b0405d82bbe9f71c9f6
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-10-26 15:51:16 -0400

    more colors

--- src/components/ShoeColors.js
+++ src/components/ShoeColors.js
@@ -3,15 +3,28 @@ import React from "react";
 function ShoeColors({ changeColor }) {
 
     const colorsArray = [
-        "#e1e851",
-        "#8cd147",
-        "#4a9ccf",
-        "#661f45",
-        "#1e2024",
-        "#800000",
-        "#e28743",
+        "#860111",
+        "#9D0218",
+        "#B4041E",
+        "#CB0525",
+        "#FF872C",
+        "#FE612C",
+        "#FD3A2D",
+        "#F11D28",
+        "#FFA12C",
         "#FFFFFF"
     ]
+    
+    // const colorsArray = [
+    //     "#e1e851",
+    //     "#8cd147",
+    //     "#4a9ccf",
+    //     "#661f45",
+    //     "#1e2024",
+    //     "#800000",
+    //     "#e28743",
+    //     "#FFFFFF"
+    // ]
 
     // for (let i = 0; i < colorsArray.length; i++) {
     //     colorsArray[i] = changeColor;
@@ -25,14 +38,14 @@ function ShoeColors({ changeColor }) {
 
     return (
         <div className="colorContainer">
-            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#e1e851" }} value="#e1e851"></button>
-            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#8cd147" }} value="#8cd147"></button>
-            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#4a9ccf" }} value="#4a9ccf"></button>
-            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#661f45" }} value="#661f45"></button>
-            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#1e2024" }} value="#1e2024"></button>
-            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#800000" }} value="#800000"></button>
-            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#e28743" }} value="#e28743"></button>
-            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#ffffff" }} value="#ffffff"></button>
+            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#860111" }} value="#860111"></button>
+            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#9D0218" }} value="#9D0218"></button>
+            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#B4041E" }} value="#B4041E"></button>
+            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#CB0525" }} value="#CB0525"></button>
+            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#FF872C" }} value="#FF872C"></button>
+            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#FE612C" }} value="#FE612C"></button>
+            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#FD3A2D" }} value="#FD3A2D"></button>
+            <button onClick={handleClick} className="color" style={{ "backgroundColor": "#F11D28" }} value="#F11D28"></button>
         </div>
     )
 }
