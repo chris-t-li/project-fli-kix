3b6a0a0a604c8c81ae1ed7535e18d9e029a8697f
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-10-27 10:51:57 -0400

    eth sign

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,11 @@ import { NavLink } from "react-router-dom";
 import logo from "../assets/fli-kix-logo-new.png"
 
 function NavBar() {
+
     return (
         <nav>
             <ul>
-                <img src={logo} />
+                <img src={logo} alt="logo"/>
                 <NavLink exact to="/">Home</NavLink>
                 <NavLink to="/design">Design Your Own</NavLink>
                 <NavLink to="/gallery">NFK Gallery</NavLink>
