6bede7f4c8ae4f1048f0a3a6b0d880f58256d5fa
Chris Li <chris.lty07@gmail.com>
2022-11-09 23:10:50 -0500

    hosting json-server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const express = require('express')
+const server = express()
+
+server.use((req, res) => {
+  res.send("Hi, welcome to FLI-KIX")
+})
+
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => {
+  console.log('Server listening on http://localhost:' + PORT)
+})
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
